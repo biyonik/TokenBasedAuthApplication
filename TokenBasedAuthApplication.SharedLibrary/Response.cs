0d2ac943d5f8680644cec326f1290df88f4a10d6
ahmet <ahmet.altun60@gmail.com>
2023-02-04 23:07:04 +0300

    Jenerik response record yapısı hazırlandı

---
+++ TokenBasedAuthApplication.SharedLibrary/Response.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json.Serialization;
+using TokenBasedAuthApplication.SharedLibrary.DTOs;
+
+namespace TokenBasedAuthApplication.SharedLibrary;
+
+public record Response<T> where T : class
+{
+    private T? Data { get; init; }
+    private int StatusCode { get; init; }
+    private ErrorDto? Error { get; init; }
+    
+    [JsonIgnore]
+    public bool IsSuccessful { get; private init; }
+    
+    public static Response<T> Success(T data, int statusCode)
+    {
+        return new Response<T>
+        {
+            Data = data,
+            StatusCode = statusCode,
+            IsSuccessful = true
+        };
+    }
+
+    public static Response<T> Success(int statusCode)
+    {
+        return new Response<T>
+        {
+            Data = default,
+            StatusCode = statusCode,
+            IsSuccessful = true
+        };
+    }
+
+    public static Response<T> Fail(ErrorDto errorDto, int statusCode)
+    {
+        return new Response<T>
+        {
+            StatusCode = statusCode,
+            Error = errorDto,
+            IsSuccessful = false
+        };
+    }
+
+    public static Response<T> Fail(string error, int statusCode, bool isShow)
+    {
+        var errorDto = new ErrorDto(error, isShow);
+        return new Response<T>
+        {
+            Error = errorDto,
+            StatusCode = statusCode,
+            IsSuccessful = false
+        };
+    }
+}
+
