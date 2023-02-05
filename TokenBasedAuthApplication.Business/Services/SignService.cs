e91fb39b0dc32fe9225720d625eb2058a710ba88
ahmet <ahmet.altun60@gmail.com>
2023-02-05 22:29:06 +0300

    SignService yazıldı

---
+++ TokenBasedAuthApplication.Business/Services/SignService.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TokenBasedAuthApplication.Business.Services;
+
+public static class SignService
+{
+    public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+    {
+        var byteCode = Encoding.UTF8.GetBytes(securityKey);
+        return new SymmetricSecurityKey(byteCode);
+    }
+}
