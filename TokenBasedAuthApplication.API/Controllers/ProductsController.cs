269a0ae81bdcd367b4dffc71e55452bcb0abe15a
ahmet <ahmet.altun60@gmail.com>
2023-02-07 16:10:54 +0300

    ProductsController oluşturuldu

---
+++ TokenBasedAuthApplication.API/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TokenBasedAuthApplication.Core.DTOs;
+
+namespace TokenBasedAuthApplication.API.Controllers;
+
+public class ProductsController: BaseApiController
+{
+    private readonly IGenericService<Product, ProductDto> _productService;
+
+    public ProductsController(IGenericService<Product, ProductDto> productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var response = await _productService.GetAllAsync(default);
+        return await HandleResponse(response);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var response = await _productService.GetByIdAsync(id, default);
+        return await HandleResponse(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(ProductDto productDto)
+    {
+        var response = await _productService.AddAsync(productDto, default);
+        return await HandleResponse(response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(ProductDto productDto, Guid id)
+    {
+        var response = await _productService.UpdateAsync(productDto, id,default);
+        return await HandleResponse(response);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(ProductDto productDto, Guid id)
+    {
+        var response = await _productService.DeleteAsync(productDto, id,default);
+        return await HandleResponse(response);
+    }
+}
--- TokenBasedAuthApplication.Core/DTOs/ProductDto.cs
+++ TokenBasedAuthApplication.Core/DTOs/ProductDto.cs
@@ -1,10 +1,10 @@
 ﻿namespace TokenBasedAuthApplication.Core.DTOs;
 
-public class ProductDto
-{
-    public Guid Id { get; init; }
-    public string Name { get; init; }
-    public decimal Price { get; init; }
-    public int Stock { get; init; }
-    public Guid UserId { get; init; }
-}
+public sealed record ProductDto
+(
+    Guid Id,
+    string Name,
+    decimal Price,
+    int Stock,
+    Guid UserId
+);
